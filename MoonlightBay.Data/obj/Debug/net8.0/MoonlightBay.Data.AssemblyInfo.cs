6d3dbbcb1fc788100bf796765c6a5d23e6f944fc
moonlighter <moonlighter@yuchengacStudio.lan>
2024-09-18 22:38:20 +0800

    2024/9/18

--- MoonlightBay.Data/obj/Debug/net8.0/MoonlightBay.Data.AssemblyInfo.cs
+++ MoonlightBay.Data/obj/Debug/net8.0/MoonlightBay.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoonlightBay.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c76ea7c226e5214843d1ea5744b615d2a936a7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4b58e638b4213727c96fa8e4348d4d62c7621e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoonlightBay.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoonlightBay.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
